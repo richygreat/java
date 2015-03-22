b957ed27d0dc72d289c75df9639f3ace62d057d6
Richy <richygreat@gmail.com>
2015-03-22 12:39:41 +0530

    Perfect Maven

--- util/src/main/java/common/CommonUtil.java
+++ util/src/main/java/common/CommonUtil.java
@@ -6,7 +6,7 @@ import crypt.CryptUtil;
 
 public class CommonUtil {
 	public static void main(String[] args) {
-		
+		System.out.println("GITHUB is wow");
 	}
 	
 	public static String hash(MessageDigest md, String plainText) {
