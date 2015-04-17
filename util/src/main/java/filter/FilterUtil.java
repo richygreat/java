6ca04026ab93ee2650be34379f38b8f39608b95a
Richy <richygreat@gmail.com>
2015-04-17 09:10:18 +0530

    Class usage to get collection object

--- util/src/main/java/filter/FilterUtil.java
+++ util/src/main/java/filter/FilterUtil.java
@@ -30,13 +30,13 @@ public class FilterUtil<T> {
 		u.setName("Kuchi Great");
 		u.setStatus("INACTIVE");
 		allUsers.add(u);
-		Collection<User> authorizedUsers = filter(allUsers, isAuthorized);
+		Collection<User> authorizedUsers = filter(allUsers, isAuthorized, Vector.class);
 		System.out.println(authorizedUsers);
 	}
 
-	public static <T> Collection<T> filter(Collection<T> target,
-			IPredicate<T> predicate) {
-		Collection<T> result = getCollectionInstance(target);
+	public static <T, C> Collection<T> filter(Collection<T> target,
+			IPredicate<T> predicate, Class<C> clazz) {
+		Collection<T> result = getCollectionInstance(clazz);
 		for (T element : target) {
 			if (predicate.apply(element)) {
 				result.add(element);
@@ -45,17 +45,17 @@ public class FilterUtil<T> {
 		return result;
 	}
 
-	private static <T> Collection<T> getCollectionInstance(Collection<T> target) {
+	private static <T, C> Collection<T> getCollectionInstance(Class<C> clazz) {
 		Collection<T> result;
-		if(Vector.class.equals(target.getClass())) {
+		if(Vector.class.equals(clazz)) {
 			result = new Vector<T>();
-		}else if(ArrayList.class.equals(target.getClass())) {
+		}else if(ArrayList.class.equals(clazz)) {
 			result = new ArrayList<T>();
-		}else if(HashSet.class.equals(target.getClass())) {
+		}else if(HashSet.class.equals(clazz)) {
 			result = new HashSet<T>();
-		}else if(LinkedHashSet.class.equals(target.getClass())) {
+		}else if(LinkedHashSet.class.equals(clazz)) {
 			result = new LinkedHashSet<T>();
-		}else if(LinkedList.class.equals(target.getClass())) {
+		}else if(LinkedList.class.equals(clazz)) {
 			result = new LinkedList<T>();
 		}else {
 			result = new ArrayList<T>();
