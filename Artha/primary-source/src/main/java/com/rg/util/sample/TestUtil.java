7bf8fb026e3d1d4e15c550d5df992097b520b7ce
Richy <richygreat@gmail.com>
2015-11-20 23:52:16 +0530

    Due date calculator

--- Artha/primary-source/src/main/java/com/rg/util/sample/TestUtil.java
+++ Artha/primary-source/src/main/java/com/rg/util/sample/TestUtil.java
@@ -1,5 +1,8 @@
 package com.rg.util.sample;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class TestUtil {
 	public static boolean isEmpty(String str) {
 		if (str == null || str.trim().length() == 0) {
@@ -7,8 +10,18 @@ public class TestUtil {
 		}
 		return false;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println(TestUtil.isEmpty("Richy"));
+		Calendar cal = new GregorianCalendar();
+		cal.set(Calendar.DATE, 24);
+		cal.set(Calendar.MONTH, 1);
+		cal.set(Calendar.YEAR, 2015);
+		System.out.println(cal.getTime());
+		Calendar now = new GregorianCalendar();
+		now.add(Calendar.DATE, 21);
+		System.out.println(now.getTime());
+		long diff = now.getTimeInMillis() - cal.getTimeInMillis();
+		diff = diff / 1000 / 60 / 60 / 24 / 7;
+		System.out.println(diff);
 	}
 }
